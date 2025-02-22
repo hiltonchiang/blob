9b2f0d858fd7355faf00c4494810154ba0d686e6
hiltonchang <hiltonchiang@gmail.com>
2025-02-22 20:13:33 +0800

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import("next").NextConfig} */
+const output = process.env.EXPORT ? 'export' : undefined
+const basePath = process.env.BASE_PATH || undefined
+
 const nextConfig = {
+  output,
+  basePath,
+  productionBrowserSourceMaps: true,
   reactStrictMode: true,
   experimental: {
     appDir: true,
