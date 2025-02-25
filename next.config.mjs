f51fe57aae9b9373fa109b14bc5a4a0f21db99e3
hiltonchang <hiltonchiang@gmail.com>
2025-02-25 20:31:29 +0800

    next.config.mjs update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
+/** @type {import('next').NextConfig} */
+const output = process.env.EXPORT ? 'export' : undefined
+const basePath = process.env.BASE_PATH || undefined
+
 import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-	reactStrictMode: true,
+  output,
+  basePath,
+  productionBrowserSourceMaps: true,
+  reactStrictMode: true,
 };
 
 export default withMDX(config);
