9496af44662f9225ca07d35fb7e06b73d3995120
hiltonchang <hiltonchiang@gmail.com>
2025-02-25 21:51:35 +0800

    use dynamic for api/search

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,4 +1,4 @@
+'use dynamic'
 import { source } from "@/lib/source";
 import { createFromSource } from "fumadocs-core/search/server";
-export const dynamic = 'force-dynamic'
 export const { GET } = createFromSource(source);
